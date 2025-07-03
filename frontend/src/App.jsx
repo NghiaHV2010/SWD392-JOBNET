e595ffa55a321efd8537b44d05c18c8f5ad96853
khang-hm <minhkhang10901@gmail.com>
2025-07-03 22:51:00 +0700

    Before pull code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-// UploadCV.jsx
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Route, Routes } from 'react-router';
-import { useAuthStore } from './store/auth.store.js';
-import { useEffect } from 'react';
+import { Route, Routes } from "react-router";
+import { useAuthStore } from "./store/auth.store.js";
+import { useEffect } from "react";
 import { Loader } from "lucide-react";
-import ScraperPage from './pages/ScraperPage.jsx';
+import ScraperPage from "./pages/ScraperPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import UploadCVPage from "./pages/UploadCVPage.jsx";
 
 function App() {
   const { isCheckingAuth, authUser, checkAuth } = useAuthStore();
@@ -13,22 +12,22 @@ function App() {
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
-  
+
   if (isCheckingAuth && !authUser) {
-    
     return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
       </div>
-    )
+    );
   }
 
   return (
     <Routes>
-      <Route index path='/' element={<></>} />
-      <Route index path='/login' element={<></>} />
-      <Route index path='/register' element={<></>} />
-      <Route index path='/scraper' element={<ScraperPage/>} />
+      <Route index path="/" element={<HomePage />} />
+      <Route index path="/login" element={<></>} />
+      <Route index path="/register" element={<></>} />
+      <Route index path="/upload-cv" element={<UploadCVPage />} />
+      <Route index path="/scraper" element={<ScraperPage />} />
     </Routes>
   );
 }
